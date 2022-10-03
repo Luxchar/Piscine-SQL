17ce4d7ae3cb9683e75104cafabfb49a953062be
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 09:53:35 +0200

    selectallcolumnalias

--- select-all-column.sql
+++ select-all-column.sql
@@ -1,5 +1,2 @@
--- Your company asks you to list certain employee information.
--- Make a query that will display the FirstName, the LastName and the Title of all employees.
-
 SELECT FirstName, LastName, Title
 FROM employees;
--- select-all.sql
+++ select-all.sql
@@ -1,4 +1,1 @@
--- Your company asks you to list all employees.
--- Make a query that will display all the data of all the employees.
-
 SELECT * FROM employees;
