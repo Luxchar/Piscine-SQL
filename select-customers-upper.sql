60d6623e609aa26dd5952115a27e519d37b5789b
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 15:32:02 +0200

    select-customer-upper.sql

--- select-customers-upper.sql
+++ select-customers-upper.sql
@@ -1,3 +1,3 @@
-SELECT UPPER(LastName) AS LastNamUpper, FirstName FROM customers
+SELECT UPPER(LastName) AS LastNameUpper, FirstName FROM customers
 WHERE LENGTH(FirstName) > 5 AND LENGTH(LastName) > 5
 ORDER BY LastName ASC;
