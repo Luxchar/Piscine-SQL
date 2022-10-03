1aec9c7dbe8a3cff51a51924fec7ecf87987b24c
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 15:21:16 +0200

    select-invoice-items.sql

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -1,3 +1,3 @@
 SELECT invoice_items.InvoiceId, tracks.Name AS InvoiceItem, invoice_items.UnitPrice FROM invoice_items 
-INNER JOIN tracks ON tracks.TrackId = invoice_items.TrackId;
-WHERE invoice_items.InvoiceId = 10 ORDER BY Name ASC;
+INNER JOIN tracks ON tracks.TrackId = invoice_items.TrackId
+WHERE invoice_items.InvoiceId = 10 ORDER BY tracks.Name ASC;
