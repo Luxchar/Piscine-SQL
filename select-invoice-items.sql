4bc52628874f850c96ecd3b755957cab15d629ec
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 15:04:46 +0200

    select-invoice-items.sql

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -1,1 +1,3 @@
-SELECT InvoiceId, Track AS InvoiceItem, UnitPrice FROM invoice_items WHERE InvoiceId = 10 ORDER BY TrackId ASC;
+SELECT InvoiceId, Name AS InvoiceItem, UnitPrice FROM invoice_items 
+JOIN tracks ON tracks.TrackId = invoice_items.TrackId
+WHERE InvoiceId = 10 ORDER BY Name ASC;
