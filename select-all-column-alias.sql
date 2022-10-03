e0366d13ab812f4d379a7f988b71f9f57492cca5
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 09:52:18 +0200

    selectallcolumnalias

--- select-all-column-alias.sql
+++ select-all-column-alias.sql
@@ -1,5 +1,2 @@
--- Your company asks you to list certain employee information.
--- Make a query that will display the EmployeeId with alias Identifiant, the LastName with alias Nom and theFirstName with alias Prenom of all employees.
-
 SELECT EmployeeId AS Identifiant, LastName AS Nom, FirstName AS Prenom
 FROM employees;
