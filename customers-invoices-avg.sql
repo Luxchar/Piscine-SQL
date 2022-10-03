834dcfd8aa92ea70d107b9ac3f5aa92b2fe7c72c
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 14:07:36 +0200

    customers-invoices-avg.sql

--- customers-invoices-avg.sql
+++ customers-invoices-avg.sql
@@ -1,2 +1,2 @@
-SELECT FirstName, LastName, AVG(Total) AS InvoicesAverage FROM Customer
+SELECT FirstName, LastName, AVG(Total) AS InvoicesAverage FROM customers
 INNER JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId GROUP BY Customer.CustomerId ORDER BY FirstName ASC;
