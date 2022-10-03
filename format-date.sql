0a724616ceb7120d7c976cf3e7e7f840f7055c57
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-03 17:24:10 +0200

    format-date.sql

--- format-date.sql
+++ format-date.sql
@@ -1,1 +1,16 @@
-SELECT EmployeeId || ' ' || LastName || ' ' || FirstName || ' ' || HireDate AS HireFrenchDate FROM employees ORDER BY HireDate ASC;
+-- In SQL, it is possible to format a date.
+
+-- Select employees information with these columns :
+
+--     EmployeeId
+--     LastName
+--     FirstName
+--     HireDate with alias HireFrenchDate
+
+-- The HireFrenchDate must be formatted in french format.
+
+-- You must order results by date in ascending order.
+
+-- French format : 15/04/2020 (day/month/year)
+
+SELECT EmployeeId, LastName, FirstName, strftime('%d/%m/%Y', HireDate) AS HireFrenchDate FROM employees ORDER BY HireDate ASC;
