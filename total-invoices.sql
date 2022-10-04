364f9c4aab3542410cafaf284c82fe3fc76e317b
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-04 09:27:39 +0200

    total-invoices.sql

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,5 +1,5 @@
-SELECT FirstName || ' ' || LastName AS FullName, SUM(Total) AS AllInvoices FROM Customer
-INNER JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
-GROUP BY Customer.CustomerId
+SELECT FirstName || ' ' || LastName AS FullName, SUM(Total) AS AllInvoices FROM customers
+INNER JOIN invoices ON customers.CustomerId = invoices.CustomerId
+GROUP BY customers.CustomerId
 HAVING AllInvoices > 38
 ORDER BY FullName ASC;
