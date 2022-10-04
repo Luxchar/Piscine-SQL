474e0ab5e79672c29bac3b633ea2f9e1d519de1b
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-04 09:04:57 +0200

    tracks-count.sql

--- tracks-count.sql
+++ tracks-count.sql
@@ -1,4 +1,3 @@
 SELECT genres.Name, COUNT(tracks.Name) AS NumberOfTracks FROM genres
 INNER JOIN tracks ON genres.GenreId = tracks.GenreId
-GROUP BY genres.Name
-ORDER BY NumberOfTracks DESC;
+GROUP BY genres.Name;
