a3219a7a95d05217eb30e48e2ecd8afb2d622aba
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-04 10:38:15 +0200

    invoices-price-case.sql

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,6 +1,4 @@
-SELECT Total, InvoiceId 
-CASE 
+SELECT CASE 
 WHEN Total < 5 THEN 'Price lower than 5$' 
 WHEN Total < 10 THEN 'Price lower than 10$' 
-ELSE 'Price greater than 10$' END AS 'CASE' FROM invoices LIMIT 100;
-
+ELSE 'Price greater than 10$' END AS Price, InvoiceId, Total FROM invoices LIMIT 100;
