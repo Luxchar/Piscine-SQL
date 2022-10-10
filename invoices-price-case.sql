34c801314a5b6d97256c24167d85d54bbeb13311
LCHARLOPEAU <luc.charlopeau@ynov.com>
2022-10-10 10:18:04 +0200

    git issue

--- invoices-price-case.sql
+++ invoices-price-case.sql
@@ -1,4 +1,8 @@
-SELECT CASE 
-WHEN Total < 5 THEN 'Price lower than 5$' 
-WHEN Total < 10 THEN 'Price lower than 10$' 
-ELSE 'Price greater than 10$' END AS Price, InvoiceId, Total FROM invoices LIMIT 100;
+SELECT InvoiceId, Total,
+CASE
+WHEN TOTAL < 5  THEN  'Price lower than 5$' 
+WHEN TOTAL < 10 THEN 'Price lower than 10$'
+WHEN  TOTAL > 10 THEN 'Price greater than 10$'
+END as 'CASE'
+FROM invoices 
+LIMIT 100;
