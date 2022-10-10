fa937a5bf375e3a772c81b28a8819c0d74fa4e12
Luxchar <luccharlopeau@gmail.com>
2022-10-10 14:09:10 +0200

    artists-bytes.sql

--- artists-bytes.sql
+++ artists-bytes.sql
@@ -1,4 +1,4 @@
-SELECT ar.Name AS ArtistName, al.Title AS AlbumName, tr.Name AS TrackName, ROUND(tr.Bytes / 1000.0 / 1000.0, 2) AS MegaBytes
+SELECT ar.Name AS ArtistName, al.Title AS AlbumName, tr.Name AS TrackName, ROUND(tr.Bytes / 1000.0 / 1000.0, 2)+"MB" AS MegaBytes
 FROM tracks tr
 INNER JOIN albums al ON tr.AlbumId = al.AlbumId
 INNER JOIN artists ar ON al.ArtistId = ar.ArtistId
