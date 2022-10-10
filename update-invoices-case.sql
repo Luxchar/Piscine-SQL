2eca33bf5b728cab55bbc7ec6e7682a527e5747f
Luxchar <luccharlopeau@gmail.com>
2022-10-10 10:56:16 +0200

    update-invoices-case.sql

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -2,4 +2,5 @@ SELECT CASE
     WHEN BillingCountry = 'France' THEN Total + 3
     WHEN BillingCountry = 'USA' THEN Total + 5
     ELSE Total + 1
-    END AS Total
+    END AS Total 
+FROM invoices;
