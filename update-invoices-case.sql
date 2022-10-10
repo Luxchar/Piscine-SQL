2abbc9479180ff32baf63aa280d3248808098ffa
Luxchar <luccharlopeau@gmail.com>
2022-10-10 11:40:59 +0200

    update-invoices-case.sql

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -1,6 +1,6 @@
-SELECT CASE 
-    WHEN `BillingCountry` = 'France' THEN `Total` + 3
-    WHEN `BillingCountry` = 'USA' THEN `Total` + 5
-    ELSE `Total` + 1
-    END AS Total 
-FROM invoices;
+UPDATE invoices 
+SET Total = CASE
+WHEN BillingCountry = 'France' THEN Total + 3
+WHEN BillingCountry = 'USA' THEN Total + 5
+ELSE Total + 1
+END
