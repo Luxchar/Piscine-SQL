aec07c04aaad1eea01f23c7266b4fb0eff6103f3
Luxchar <luccharlopeau@gmail.com>
2022-10-18 09:53:25 +0200

    report-to.sql

--- report-to.sql
+++ report-to.sql
@@ -1,4 +1,4 @@
-SELECT e1.employee_id, e1.first_name || ' ' || e1.last_name AS employee_name, e2.first_name || ' ' || e2.last_name AS manager_name
+SELECT e1.employeeid, e1.firstname || ' ' || e1.lastname AS EmployeeName, e2.first_name || ' ' || e2.last_name AS ReportsTo
 FROM employees e1
 INNER JOIN employees e2 ON e1.reports_to = e2.employee_id
 ORDER BY e1.employee_id ASC;
