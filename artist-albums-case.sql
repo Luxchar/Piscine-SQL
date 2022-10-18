6abb6c481eaeca1e11315fbff79d078dd61c1bfa
Luxchar <luccharlopeau@gmail.com>
2022-10-18 09:18:19 +0200

    artists-bytes.sql

--- artist-albums-case.sql
+++ artist-albums-case.sql
@@ -1,8 +1,8 @@
 SELECT ar.Name, COUNT(al.AlbumId) AS NbAlbums,
     CASE
         WHEN COUNT(al.AlbumId) = 1 THEN 'Unproductive'
-        WHEN COUNT(al.AlbumId) > 1 AND COUNT(al.AlbumId) < 10 THEN 'Productive'
         WHEN COUNT(al.AlbumId) >= 10 THEN 'Very Productive'
+        WHEN COUNT(al.AlbumId) > 1 THEN 'Productive'
     END AS IsProductive
 FROM artists ar
 INNER JOIN albums al ON ar.ArtistId = al.ArtistId
