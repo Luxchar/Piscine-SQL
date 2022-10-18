d22ec388a986738cbda4a4ba17d1e77703b51189
Luxchar <luccharlopeau@gmail.com>
2022-10-18 10:04:27 +0200

    report-to.sql

--- report-to.sql
+++ report-to.sql
@@ -1,4 +1,4 @@
-SELECT e1.employeeid, e1.firstname || ' ' || e1.lastname AS employee_name, e2.firstname || ' ' || e2.lastname AS manager_name
+SELECT e1.employeeid, e1.firstname || ' ' || e1.lastname AS EmployeeName, e2.firstname || ' ' || e2.lastname AS ReportsTo
 FROM employees e1
-OUTER JOIN employees e2 ON e1.reportsto = e2.employeeid
+LEFT OUTER JOIN employees e2 ON e1.reportsto = e2.employeeid
 ORDER BY e1.employeeid ASC;
